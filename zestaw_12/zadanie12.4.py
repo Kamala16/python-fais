caa6a5293329229afd695d22965a962ffb54003a
Aleksandra Chrzanowska <aleksandra.chrzanowska@student.uj.edu.pl>
2020-01-15 11:21:25 +0100

    poprawione zadanie 12.4

--- zestaw_12/zadanie12.4.py
+++ zestaw_12/zadanie12.4.py
@@ -1,35 +1,57 @@
 import random
 
-def moda():
-    L = ListCreate()
-    iloscM = 1
-    maxM = 0
-    for item in range (0, len(L)-1):
-        moda = L[item]
-        if L[item] == L[item + 1]:
-            iloscM += 1
-            maxM = iloscM
+def moda_sort(L, left, right):
+    if left+1 > right:
+        return None
+    mode = None
+    modeNumber = 0
+    temp = left
+    tempNumber = 0
+    while temp < right:
+        temp += 1
+        if L[temp] == L[temp-1]:
+            tempNumber = tempNumber + 1
+            if tempNumber > modeNumber:
+                modeNumber = tempNumber
+                mode = temp
         else:
-            if maxM < iloscM:
-                maxM = iloscM
-                moda = L[item]
-            iloscM = 1
-    print(" moda to {}".format(moda))
+            tempNumber = 1
+    return mode
 
-def ListCreate():
-    k = int(input("podaj zakres liczb naturalnych: "))
-    ilosc = int(input("podaj ilość liczb: "))
+def ListCreate(k, ilosc):
     L = []
     for item in range (0, ilosc):
         rand = int(random.uniform(0, k-1))
         L.append(rand)
-    bubbleSort(L)
+    quicksort(L, 0, len(L)-1)
     return L
 
-def bubbleSort(L):
-    for itemFirst in range (0, len(L)):
-        for itemSecond in range (0, len(L)-1):
-            if L[itemSecond] > L[itemFirst]:
-                L[itemFirst], L[itemSecond] = L[itemSecond], L[itemFirst]
+def quicksort(L, left, right):
+    """Sortowanie szybkie wg Cormena str. 169."""
+    if left >= right:
+        return
+    pivot = partition(L, left, right)
+    quicksort(L, left, pivot - 1)
+    quicksort(L, pivot + 1, right)
 
-print(moda())
+def partition(L, left, right):
+    """Zwraca indeks elementu rozdzielającego."""
+    x = L[right]
+    i = left
+    for j in range(left, right):
+        if L[j] <= x:
+            swap(L, i, j)
+            i += 1
+    swap(L, i, right)
+    return i
+
+def swap(L, left, right):
+    """Zamiana miejscami dwóch elementów."""
+    L[left], L[right] = L[right], L[left]
+
+k = int(input("podaj zakres liczb naturalnych: "))
+ilosc = int(input("podaj ilość liczb: "))
+
+Lista = ListCreate(k, ilosc)
+print(Lista)
+print(Lista[moda_sort(Lista, 0, len(Lista)-1)])
